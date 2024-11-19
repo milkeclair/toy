8a0684b80ed79c6835bfdb2f14c79088ae1da00f
milkeclair <milkeclair.black@gmail.com>
2024-11-19 11:26:35 +0900

    feat: ページ配信を動的に設定、ページがなければ404を表示

--- snippets/javascript/server/node_server.js
+++ snippets/javascript/server/node_server.js
@@ -1,15 +1,30 @@
 import { createServer } from "node:http";
 import fs from "node:fs";
+import ejs from "ejs";
 
 const hostname = "localhost";
 const port = 3000;
-const html = fs.readFileSync("../compare_code.html");
+const rootPath = "..";
+const mainPagePath = `${rootPath}/compare_code.html`;
 
 const server = createServer((request, response) => {
   console.log(`Request from: url=${request.url}, method=${request.method}`);
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/html");
-  response.end(html);
+
+  if (request.url === "/favicon.ico") {
+    const view = renderView("./favicon.ico");
+    response.statusCode = isNotFound(view) ? 404 : 200;
+    response.setHeader("Content-Type", "image/x-icon");
+    response.end(view);
+  } else {
+    const view =
+      request.url === "/"
+        ? renderView(mainPagePath)
+        : renderView(`${rootPath}${request.url}.html`);
+
+    response.statusCode = isNotFound(view) ? 404 : 200;
+    response.setHeader("Content-Type", "text/html");
+    response.end(view);
+  }
 });
 
 server.listen(port, hostname, () => {
@@ -21,3 +36,17 @@ process.on("SIGINT", () => {
   console.log("\nGoodbye!");
   process.exit();
 });
+
+const renderView = (path) => {
+  try {
+    return fs.readFileSync(path, "utf-8");
+  } catch {
+    return ejs.render(fs.readFileSync("./404.html.ejs", "utf-8"), {
+      message: `${path} not found`,
+    });
+  }
+};
+
+const isNotFound = (view) => {
+  return view.includes("<title>404</title>");
+};
