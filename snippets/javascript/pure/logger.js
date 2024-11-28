9b304ab35d73e92fdbcc6205bf5ae9c9d103c8c7
milkeclair <milkeclair.black@gmail.com>
2024-11-28 21:27:58 +0900

    feat: debugを追加

--- snippets/javascript/pure/logger.js
+++ snippets/javascript/pure/logger.js
@@ -9,6 +9,10 @@ export default class Logger {
     Logger.#p("ok", message, { color: "green", ...options });
   }
 
+  static debug(message, options = {}) {
+    Logger.#p("debug", message, { color: "blue", ...options });
+  }
+
   static warn(message, options = {}) {
     Logger.#p("warn", message, { color: "yellow", ...options });
   }
@@ -20,6 +24,7 @@ export default class Logger {
   static returnable = {
     info: (message, options = {}) => Logger.info(message, { ...options, returnable: true }),
     ok: (message, options = {}) => Logger.ok(message, { ...options, returnable: true }),
+    debug: (message, options = {}) => Logger.debug(message, { ...options, returnable: true }),
     warn: (message, options = {}) => Logger.warn(message, { ...options, returnable: true }),
     error: (message, options = {}) => Logger.error(message, { ...options, returnable: true }),
   };
@@ -27,6 +32,7 @@ export default class Logger {
   constructor() {
     this.info = Logger.info;
     this.ok = Logger.ok;
+    this.debug = Logger.debug;
     this.warn = Logger.warn;
     this.error = Logger.error;
     this.returnable = Logger.returnable;
