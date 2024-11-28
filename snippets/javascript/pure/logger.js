bc8e2e377342bcf10a3a4ff9f9fbedb7dbdfee06
milkeclair <milkeclair.black@gmail.com>
2024-11-28 19:46:13 +0900

    feat: ログを出さずに文字列を返す処理を追加

--- snippets/javascript/pure/logger.js
+++ snippets/javascript/pure/logger.js
@@ -13,15 +13,30 @@ export default class Logger {
     Logger.#p("error", message, color, before, after, timestamp);
   }
 
+  static returnable = {
+    info: (message, { color = "white", before = false, after = false, timestamp = true } = {}) => {
+      Logger.#p("info", message, color, before, after, timestamp, true);
+    },
+
+    warn: (message, { color = "yellow", before = false, after = false, timestamp = true } = {}) => {
+      Logger.#p("warn", message, color, before, after, timestamp, true);
+    },
+
+    error: (message, { color = "red", before = false, after = false, timestamp = true } = {}) => {
+      Logger.#p("error", message, color, before, after, timestamp, true);
+    },
+  };
+
   constructor() {
     this.info = Logger.info;
     this.warn = Logger.warn;
     this.error = Logger.error;
+    this.returnable = Logger.returnable;
   }
 
   // private
 
-  static #p = (level, message, color, before, after, timestamp) => {
+  static #p = (level, message, color, before, after, timestamp, returnable = false) => {
     const time = new Date().toLocaleString("sv-SE");
     if (timestamp) message = `${time} - ${message}`;
 
@@ -29,6 +44,7 @@ export default class Logger {
     if (before) text = `\n${text}`;
     if (after) text = `${text}\n`;
 
+    if (returnable) return text;
     console.log(text);
   };
 }
