fad0284ac95f24fb61816cb8a1f172add53e9236
milkeclair <milkeclair.black@gmail.com>
2024-11-28 19:32:59 +0900

    feat: タイムスタンプを表示できるように変更

--- snippets/javascript/pure/logger.js
+++ snippets/javascript/pure/logger.js
@@ -1,16 +1,16 @@
 import { styleText } from "node:util";
 
 export default class Logger {
-  static info(message, { color = "white", before = false, after = false } = {}) {
-    Logger.#p("info", message, color, before, after);
+  static info(message, { color = "white", before = false, after = false, timestamp = true } = {}) {
+    Logger.#p("info", message, color, before, after, timestamp);
   }
 
-  static warn(message, { color = "yellow", before = false, after = false } = {}) {
-    Logger.#p("warn", message, color, before, after);
+  static warn(message, { color = "yellow", before = false, after = false, timestamp = true } = {}) {
+    Logger.#p("warn", message, color, before, after, timestamp);
   }
 
-  static error(message, { color = "red", before = false, after = false } = {}) {
-    Logger.#p("error", message, color, before, after);
+  static error(message, { color = "red", before = false, after = false, timestamp = true } = {}) {
+    Logger.#p("error", message, color, before, after, timestamp);
   }
 
   constructor() {
@@ -21,10 +21,14 @@ export default class Logger {
 
   // private
 
-  static #p = (level, message, color, before, after) => {
+  static #p = (level, message, color, before, after, timestamp) => {
+    const time = new Date().toLocaleString("sv-SE");
+    if (timestamp) message = `${time} - ${message}`;
+
     let text = styleText(color, `[${level}] ${message}`);
     if (before) text = `\n${text}`;
     if (after) text = `${text}\n`;
+
     console.log(text);
   };
 }
